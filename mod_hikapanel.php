6c731578f2cda9a57a98863242fea6c99a91932e
bolando <biuro@solmedia.pl>
2017-08-22 10:24:24 +0200

    wgranie plików

---
+++ mod_hikapanel.php
@@ -0,0 +1,6 @@
+<?php
+// blokowanie bezpośredniego uruchomienia
+defined('_JEXEC') or die;
+// włączenie klasy modelu z jej metodami
+//włączenie domyślnego layoutu
+require JModuleHelper::getLayoutPath('mod_hikapanel');
---
+++ tmpl/default.php
@@ -0,0 +1,42 @@
+<?php 
+// No direct access
+defined('_JEXEC') or die; 
+
+$doc->addStyleSheet('modules/mod_hikapanel/css/style.css');?>
+ <div id="cpanel">
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&amp;ctrl=product" title="" data-toggle="hk-tooltip" data-title="Dodaj/Edytuj/Usuń Produkty&lt;br/&gt;Zarządzaj zdjęciami, plikami, cenami, produktami powiązanymi, etc&lt;br/&gt;Możesz r&oacute;wnież zarządzać informacją o twoich produktach w oparciu o charakterystyki" data-original-title=""> <span class="icon-48-product" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Produkty</span> </a></div>
+</div>
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&amp;ctrl=category" title="" data-toggle="hk-tooltip" data-title="Dodaj/Edytuj/Usuń Kategorie Produkt&oacute;w" data-original-title=""> <span class="icon-48-categories" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Kategorie</span> </a></div>
+</div>
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&amp;ctrl=user" title="" data-toggle="hk-tooltip" data-title="Zarządzaj i kontaktuj się ze swoimi klientami&lt;br/&gt;Będziesz m&oacute;gł zarządzać ich adresami i zam&oacute;wieniami" data-original-title=""> <span class="icon-48-user" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Klienci</span> </a></div>
+</div>
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&amp;ctrl=order" title="" data-toggle="hk-tooltip" data-title="Zarządzaj zam&oacute;wieniami Twoich klient&oacute;w&lt;br/&gt;Dodaj/Edytuj/Usuń Statusy Zam&oacute;wień" data-original-title=""> <span class="icon-48-order" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Zam&oacute;wienia</span> </a></div>
+</div>
+
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&amp;ctrl=discount" title="" data-toggle="hk-tooltip" data-title="Dodaj/Edytuj/Usuń Kody Rabatowe&lt;br/&gt;Zarządzaj ograniczeniami do nich" data-original-title=""> <span class="icon-48-discount" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Rabaty</span> </a></div>
+</div>
+
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&ctrl=characteristic" title="" data-toggle="hk-tooltip" data-title="Edytuj łatwo każdy widok w Sklepie&lt;br/&gt;Konfiguruj menu zawartości oraz moduły&lt;br/&gt;Konfiguracja P&oacute;l Użytkownika" data-original-title=""> <span class="icon-48-view" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Kolory i rozmiar</span> </a></div>
+</div>
+<div class="icon-wrapper">
+			<div class="icon">
+				<a href="/administrator/index.php?option=com_hikashop&ctrl=field" data-toggle="hk-tooltip"  data-original-title="" title="">
+					<span class="icon-48-plugin" style="background-repeat:no-repeat;background-position:center;height:48px;padding:10px 0;"></span>
+					<span>Materiał</span>
+				</a>
+			</div>
+		</div>
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&amp;ctrl=config" title="" data-toggle="hk-tooltip" data-title="Konfiguracja HikaShop&lt;br/&gt;Modyfikacja jakiegokolwiek łańcucha znakowego użytego w HikaShop&lt;br/&gt;Modyfikacja jakiegokolwiek e-maila użytego w HikaShop&lt;br/&gt;Włącz/Wyłącz Dodatki HikaShop" data-original-title=""> <span class="icon-48-config" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Konfiguracja</span> </a></div>
+</div>
+<div class="icon-wrapper">
+<div class="icon"><a href="/administrator/index.php?option=com_hikashop&amp;ctrl=documentation" title="" data-toggle="hk-tooltip" data-title="Wyświetl pomoc HikaShop&lt;br/&gt;Sprawdź aktualizacje i historię zmian&lt;br/&gt;Dostęp do forum w celu uzyskania dalszej pomocy" data-original-title=""> <span class="icon-48-install" style="background-repeat: no-repeat; background-position: center; height: 48px; padding: 10px 0;"></span> <span>Aktualizacja &amp; Info</span> </a></div>
+</div>
+<div style="clear: both;">&nbsp;</div>
+</div>
